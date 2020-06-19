7992565c0d5f56e652c7716a526ac4ea2acf9e65
regisbr13 <regislima1391@gmail.com>
2020-06-18 23:46:44 -0300

    Inicio do Setup da camada de identidade, 1a migration realizada

---
+++ src/services/NSE.Identity.API/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace NSE.Identity.API.Data
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    }
+}
