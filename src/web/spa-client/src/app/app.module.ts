943782f720a0c9b997f02922b737bb11239fe055
Ivan Alves <ivan.alves@dellead.com>
2020-08-21 13:03:22 -0300

    wip: estrutura css, html, e home

--- src/web/spa-client/src/app/app.module.ts
+++ src/web/spa-client/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
